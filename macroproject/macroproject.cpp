07229ee30e60833885b49dac061e2bda7d8605f8
100dlswjd <82484962+100dlswjd@users.noreply.github.com>
2021-08-04 21:25:16 +0900

    macro_project_01

---
+++ macroproject/macroproject.cpp
@@ -0,0 +1,73 @@
+#include<windows.h>
+
+LRESULT CALLBACK WndProc(HWND, UINT, WPARAM, LPARAM);
+HINSTANCE g_hInst;
+HWND hWndMain;
+LPCTSTR lpszClass = TEXT("macro_project");
+
+int APIENTRY WinMain(HINSTANCE hInstance, HINSTANCE hPrevInstance, LPSTR lpszCmdParam, int nCmdShow) {
+	HWND hWnd;
+	MSG Message;
+	WNDCLASS WndClass;
+	g_hInst = hInstance;
+
+	WndClass.cbClsExtra = 0;
+	WndClass.cbWndExtra = 0;
+	WndClass.hbrBackground = (HBRUSH)GetStockObject(COLOR_WINDOW + 1);
+	WndClass.hCursor = LoadCursor(NULL, IDC_ARROW);
+	WndClass.hIcon = LoadIcon(NULL, IDI_APPLICATION);
+	WndClass.hInstance = hInstance;
+	WndClass.lpfnWndProc = WndProc;
+	WndClass.lpszClassName = lpszClass;
+	WndClass.lpszMenuName = NULL;
+	WndClass.style = CS_HREDRAW | CS_VREDRAW;
+	RegisterClass(&WndClass);
+
+	hWnd = CreateWindow(lpszClass, lpszClass, WS_OVERLAPPEDWINDOW, CW_USEDEFAULT, CW_USEDEFAULT, CW_USEDEFAULT, CW_USEDEFAULT, NULL, (HMENU)NULL, hInstance, NULL);
+
+	ShowWindow(hWnd, nCmdShow);
+
+	while (GetMessage(&Message, NULL, 0, 0)) {
+		TranslateMessage(&Message);
+		DispatchMessage(&Message);
+	}
+	return (int)Message.wParam;
+}
+
+LRESULT CALLBACK WndProc(HWND hWnd, UINT iMessage, WPARAM wParam, LPARAM lParam) {
+	HDC hdc;
+	PAINTSTRUCT ps;
+	int x = GetSystemMetrics(SM_CXSCREEN) / 2;
+	int y = GetSystemMetrics(SM_CYSCREEN) / 2;
+
+	switch (iMessage) {
+	case WM_CREATE:
+		return 0;
+	case WM_KEYDOWN:
+		switch (wParam) {
+		case VK_LEFT:
+			SetCursorPos(x-5,y);
+			x = x - 5;
+			//MessageBox(hWnd, ,TEXT("screen size"), MB_OK)
+			return 0;
+		case VK_UP:
+			SetCursorPos(x, y-5);
+			y = y - 5;
+			//MessageBox(hWnd, TEXT("措面 郴侩"), TEXT("措面 力格"), MB_OKCANCEL);
+			return 0;
+		case VK_RIGHT:
+			SetCursorPos(x+5, y);
+			x = x + 5;
+			return 0;
+		case VK_DOWN:
+			SetCursorPos(x, y+5);
+			y = y + 5;
+			return 0;
+		}
+	case WM_DESTROY:
+		PostQuitMessage(0);
+		return 0;
+	}
+	return(DefWindowProc(hWnd, iMessage, wParam, lParam));
+}
+
